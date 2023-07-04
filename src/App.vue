1eaca71072e54dc1a5fa7015945c066971bacccf
Jeka <jonrovniy@yandex.ru>
2023-07-04 20:41:19 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/logo.png" alt="logo" class="h-9">
+    </a>
+    <a href="#">
+      Day Complete!
+    </a>
+  </header>
+  <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita est nihil suscipit, 
+      reiciendis quos nulla nobis architecto earum itaque consequatur laboriosam eius mollitia
+      optio vitae, delectus quis, non ut consectetur. Lorem ipsum dolor sit amet consectetur adipisicing
+       elit. Necessitatibus, doloremque at deleniti quam quaerat ea, optio sapiente in rem omnis, temporibus
+        excepturi ex hic voluptate ipsum? Velit error cumque aspernatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita est nihil suscipit, 
+      reiciendis quos nulla nobis architecto earum itaque consequatur laboriosam eius mollitia
+      optio vitae, delectus quis, non ut consectetur. Lorem ipsum dolor sit amet consectetur adipisicing
+       elit. Necessitatibus, doloremque at deleniti quam quaerat ea, optio sapiente in rem omnis, temporibus
+        excepturi ex hic voluptate ipsum? Velit error cumque aspernatur.
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
